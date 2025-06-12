a9fd835d1c16630e4e0a134919766498d43e064b
Rey Ebanio <reyebanio123@gmail.com>
2025-06-13 00:47:41 +0800

    reviews

--- src/review/JavaTimeDates.java
+++ src/review/JavaTimeDates.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class JavaTimeDates {
 	public static void main(String[] args) {
@@ -14,15 +15,23 @@ public class JavaTimeDates {
 		LocalTime localTime = LocalTime.of(10, 30);
 		LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
 		ZoneId zId = ZoneId.of("Asia/Manila");
-		//ZoneId zId2 = ZoneId.of("United Kingdom");
-		ZonedDateTime zdt = ZonedDateTime.of(localDateTime, zId);
-		Instant instant = zdt.toInstant();
+		ZoneId zId2 = ZoneId.of("America/Cuiaba");
 		
-		var zones=ZoneId.getAvailableZoneIds();
-		for(String zone:zones) {
-			System.out.println(ZonedDateTime.of(localDateTime, ZoneId.of(zone)));
-			// the toInstant api transforms it to a UTC/GMT time
-			System.out.println(ZonedDateTime.of(localDateTime, ZoneId.of(zone)).toInstant());
-		}
+		
+		ZonedDateTime zdt = ZonedDateTime.of(localDateTime, zId);
+		ZonedDateTime zdt2 = ZonedDateTime.of(localDateTime, zId2);
+		
+		ZonedDateTime zdtOffset= zdt.plus(1, ChronoUnit.HOURS);
+		
+		System.out.println(zdt2.getOffset());
+		
+//		Instant instant = zdt.toInstant();
+//		
+//		var zones=ZoneId.getAvailableZoneIds();
+//		for(String zone:zones) {
+//			System.out.println(ZonedDateTime.of(localDateTime, ZoneId.of(zone)));
+//			// the toInstant api transforms it to a UTC/GMT time
+//			System.out.println(ZonedDateTime.of(localDateTime, ZoneId.of(zone)).toInstant());
+//		}
 	}
 }
